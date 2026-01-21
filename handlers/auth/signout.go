693066c39f994b54815ff64389266b87ff881039
srv-kirim <inbox.kirim@gmail.com>
2026-01-21 20:40:59 +0700

    refactor

--- handlers/auth/signout.go
+++ handlers/auth/signout.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -11,34 +10,31 @@ import (
 )
 
 func (u *domainHandler) Signout(c echo.Context) error {
-	// Hapus token cookie
-	cookies := http.Cookie{
-		Name:     "token",                             // Nama cookie yang digunakan untuk token
-		Value:    "",                                  // Setel nilai cookie menjadi kosong
-		Path:     "/dashboard",                        // Path di mana cookie ini berlaku
-		Expires:  time.Now().Add(-7 * 24 * time.Hour), // Setel waktu kadaluarsa untuk menghapus cookie
-		MaxAge:   -1,                                  // MaxAge negatif untuk menghapus cookie segera
-		HttpOnly: true,                                // Hanya dapat diakses melalui HTTP (bukan JavaScript)
-	}
-	http.SetCookie(c.Response().Writer, &cookies)
 
-	// Hapus refresh_token cookie jika diperlukan
-	cookiesRefresh := http.Cookie{
-		Name:     "refresh_token", // Nama cookie untuk refresh_token
+	// Hapus access token
+	http.SetCookie(c.Response().Writer, &http.Cookie{
+		Name:     "token",
 		Value:    "",
-		Path:     "/dashboard", // Path yang sama
-		Expires:  time.Now().Add(-7 * 24 * time.Hour),
+		Path:     "/",
+		Domain:   ".cashpay.co.id",
 		MaxAge:   -1,
-		HttpOnly: true,
-	}
-	http.SetCookie(c.Response().Writer, &cookiesRefresh)
+		Expires:  time.Unix(0, 0),
+		Secure:   true,
+		SameSite: http.SameSiteNoneMode,
+		// ❌ HttpOnly false atau dihapus
+	})
 
-	// Kirimkan response sukses (tidak ada data tambahan)
-	err := res.SuccessResponse(nil).Send(c)
-	if err != nil {
-		// Log error jika gagal mengirimkan response
-		fmt.Println("Error sending response:", err)
-	}
+	// Hapus refresh token
+	http.SetCookie(c.Response().Writer, &http.Cookie{
+		Name:     "refresh_token",
+		Value:    "",
+		Path:     "/",
+		Domain:   ".cashpay.co.id",
+		MaxAge:   -1,
+		Expires:  time.Unix(0, 0),
+		Secure:   true,
+		SameSite: http.SameSiteNoneMode,
+	})
 
-	return err
+	return res.SuccessResponse(nil).Send(c)
 }
